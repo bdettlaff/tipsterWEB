639e899597426cde45ff15ba3744521e5c416b1c
Bartłomiej Dettlaff <bartlomiejdettlaff@interia.pl>
2020-02-17 23:46:55 +0100

    Poprawki do serwera

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(__dirname + '/dist/tipster-web'));
+app.use(express.static(__dirname + '/dist/tipsterWEB'));
 
 app.get('/*', function(req,res) {
 
-  res.sendFile(path.join(__dirname+'/dist/tipster-web/index.html'));
+  res.sendFile(path.join(__dirname+'/dist/tipsterWEB/index.html'));
 });
 
 
